54e8b298e1ff4006cbf0ee2039cd35159ae740ad
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2020-10-03 22:32:05 -0400

    Updating docker run scripts

--- dockerRunScripts/run_linux.sh
+++ dockerRunScripts/run_linux.sh
@@ -10,7 +10,8 @@ NC='\033[0m' # No Colour
 printf "\n [+] ${BIGreen} Hello ${USER}! Setting up your working container.${NC}\n [+]  ${BIGreen}You may be asked to enter your password.${NC}\n"
 
 # Run container:
-sudo docker run --rm -it -v $PWD:/root devi
+sudo docker run --rm -it -p 8501:8501 -v $PWD:/root devi
+
 
 printf "\n  [+]  ${BIGreen} Returning file ownership from root to ${USER}.\n       You may be asked to reenter your password.${NC}\n"
 # change ownership of a file to your own login if it was created in the docker container.
